9dfc147332f4e1a4d1efd9bdc08feaf2acab2256
Philipp Oppermann <dev@phil-opp.com>
2015-01-01 19:09:32 +0100

    add schedule; reschedule and schedule both call schedule_inner

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -11,53 +11,38 @@ pub fn spawn<F, R>(f:F) -> Future<R> where F: FnOnce()->R, F:Send {
 }
 
 pub unsafe fn reschedule(current_rsp: uint) -> ! {
-
     // switch stack so we can park current thread
-    const SCHEDULER_STACK_SIZE: uint = 4096;
-    static SCHEDULER_STACK: [u8; SCHEDULER_STACK_SIZE] = [0; SCHEDULER_STACK_SIZE];
 
-    let scheduler_stack_top = &SCHEDULER_STACK as *const [u8; 4096] as uint
-        + SCHEDULER_STACK_SIZE;
-    call_on_stack(inner, current_rsp, scheduler_stack_top);
-
-    fn new_stack() -> uint {
-        const NEW_STACK_SIZE: uint = 4096*2;
-        let stack_bottom = unsafe{allocate(NEW_STACK_SIZE, 4096)};
-        let stack_top = (stack_bottom as uint + NEW_STACK_SIZE);
-        stack_top
-    }
-
-    fn call_on_stack<Arg>(function: fn(Arg) -> !, arg: Arg, stack_top: uint) -> ! {
-        unsafe{asm!("call $0;" :: "r"(function), "{rdi}"(arg), "{rsp}"(stack_top) :
-            : "intel", "volatile")}
-        panic!("diverging fn returned");
-    }
+    call_on_stack(inner, current_rsp, scheduler_stack_top());
 
     fn inner(current_rsp: uint) -> ! {
-        extern {
-            fn pop_registers_and_iret(rsp: uint) -> !;
-        }
-
         // park current thread
         let current = Thread::from_rsp(current_rsp);
         global().scheduler.lock().park(current);
 
-        // schedule new thread
-        loop {
-            print!("_");
-            let new_thread = unsafe{global().scheduler.lock().schedule().unwrap_or_default()};
-            match new_thread {
-                Thread{state: ThreadState::Active{rsp}} => {
-                    unsafe{pop_registers_and_iret(rsp)}
-                },
-                Thread{state: ThreadState::New{function}} => {
-                    println!("new");
-                    let new_stack_top = new_stack();
-                    let invoke_fn : fn(Box<FnBox() + Send>) -> ! = invoke;
-                    call_on_stack(invoke_fn, function, new_stack_top)
-                },
-            }
-        }
+        schedule_inner();
+    }    
+}
+
+pub unsafe fn schedule() -> ! {
+    call_on_stack(inner, (), scheduler_stack_top());
+
+    fn inner(_:()) -> ! {
+        schedule_inner();
+    }
+}
+
+fn schedule_inner() -> ! {
+    #[allow(improper_ctypes)]
+    extern {
+        fn pop_registers_and_iret(rsp: uint) -> !;
+    }
+
+    fn new_stack() -> uint {
+        const NEW_STACK_SIZE: uint = 4096*2;
+        let stack_bottom = unsafe{allocate(NEW_STACK_SIZE, 4096)};
+        let stack_top = stack_bottom as uint + NEW_STACK_SIZE;
+        stack_top
     }
 
     fn invoke(function: Box<FnBox() + Send>) -> ! {
@@ -65,7 +50,35 @@ pub unsafe fn reschedule(current_rsp: uint) -> ! {
         unsafe{asm!("int $$66" :::: "volatile")};
         unreachable!();
     }
-    
+
+    // schedule new thread
+    print!("_");
+    let new_thread = unsafe{global().scheduler.lock().schedule().unwrap_or_default()};
+
+    match new_thread {
+        Thread{state: ThreadState::Active{rsp}} => {
+            unsafe{pop_registers_and_iret(rsp)}
+        },
+        Thread{state: ThreadState::New{function}} => {
+            println!("new");
+            let new_stack_top = new_stack();
+            let invoke_fn : fn(Box<FnBox() + Send>) -> ! = invoke;
+            unsafe{call_on_stack(invoke_fn, function, new_stack_top)}
+        },
+    }
+}
+
+fn scheduler_stack_top() -> uint {
+    const SCHEDULER_STACK_SIZE: uint = 4096;
+    static SCHEDULER_STACK: [u8; SCHEDULER_STACK_SIZE] = [0; SCHEDULER_STACK_SIZE];
+
+    &SCHEDULER_STACK as *const [u8; 4096] as uint + SCHEDULER_STACK_SIZE
+}
+
+unsafe fn call_on_stack<Arg>(function: fn(Arg) -> !, arg: Arg, stack_top: uint) -> ! {
+    asm!("call $0;" :: "r"(function), "{rdi}"(arg), "{rsp}"(stack_top) :
+        : "intel", "volatile");
+    panic!("diverging fn returned");
 }
 
 pub type GlobalScheduler = Scheduler;
