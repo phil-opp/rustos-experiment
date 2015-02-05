a948d37b8b5bda8ce6e521740c3af0b37c699fc9
Philipp Oppermann <dev@phil-opp.com>
2015-02-05 18:03:31 +0100

    catch double fault explicitly

--- src/lib.rs
+++ src/lib.rs
@@ -147,6 +147,7 @@ pub extern "C" fn interrupt_handler(interrupt_number: u64, error_code: u64, sp: 
         33 => print!("k"),
         50 => panic!("out of memory"),
         66 => println!("ending thread..."),
+        8 => panic!("DOUBLE FAULT"),
         _ => panic!("unknown interrupt! number: {}, error_code: {:x}",interrupt_number, error_code),
     };
     unsafe{send_eoi(interrupt_number)};
