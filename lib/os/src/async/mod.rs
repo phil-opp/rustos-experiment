1dafd1078a1ebb407fe7dcdab63a5a7dcb8a5a62
Philipp Oppermann <dev@phil-opp.com>
2015-02-10 11:38:15 +0100

    rename stream to spsc_stream and start Stream trait in stream

--- lib/os/src/async/mod.rs
+++ lib/os/src/async/mod.rs
@@ -1,7 +1,7 @@
 pub use self::future::{Future, FutureExt};
 pub use self::computation::Computation;
 //pub use self::future_stream::Stream;
-pub use self::stream::Stream;
+pub use self::spsc_stream::Stream;
 
 pub use core_local::task_queue;
 
@@ -9,6 +9,7 @@ mod future;
 mod computation;
 //mod future_stream;
 mod stream;
+mod spsc_stream;
 mod spsc_queue;
 mod mpsc_queue;
 
